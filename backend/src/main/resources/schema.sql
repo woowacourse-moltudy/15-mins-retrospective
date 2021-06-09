4d24809b0ba3e2a0bc68afb971b9d8a4151c2e8b
pika96 <52442949+pika96@users.noreply.github.com>
2021-06-09 19:44:08 +0900

    feat(member): 스키마 추가

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-create table if not exists temp
+create table if not exists member
 (
     id bigint auto_increment not null,
-    name varchar(255) not null unique
+    name varchar(255) not null unique,
+    password varchar(255) not null
 );
