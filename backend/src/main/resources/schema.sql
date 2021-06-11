9e286103c3bfb40ee9551b080da093e70bb92cf4
y_woo :) <dusdn1702@naver.com>
2021-06-11 11:31:09 +0900

    feat: 시간 관련 기능 구현

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,5 +1,12 @@
-create table if not exists MEMBER
+create table if not exists MEMBER (
     id bigint auto_increment not null,
     name varchar(255) not null unique
-
 );
+
+create table if not exists CONFERENCE_TIME (
+    id bigint auto_increment not null,
+    time int(24) not null unique
+);
+
+insert into CONFERENCE_TIME(time) values (6);
+insert into CONFERENCE_TIME(time) values (10);
---
+++ backend/src/test/java/wooteco/retrospective/domain/time/TimeTest.java
@@ -0,0 +1,20 @@
+package wooteco.retrospective.domain.time;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TimeTest {
+
+    @DisplayName("옳지 않은 시간을 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 25})
+    void invalidTime(int time) {
+        assertThatThrownBy(() -> new Time(time))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("옳지 않은 시간입니다.");
+    }
+
+}
