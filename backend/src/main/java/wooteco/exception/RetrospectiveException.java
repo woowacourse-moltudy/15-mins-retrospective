ba61bda42c68d82f8c14cd586ab7e27707c840df
Daeun Lee <leede0418@likelion.org>
2021-06-12 14:11:34 +0900

    refactor(login): RuntimeException - Exception - Throwable getMessage() 활용

--- backend/src/main/java/wooteco/exception/RetrospectiveException.java
+++ backend/src/main/java/wooteco/exception/RetrospectiveException.java
@@ -2,14 +2,7 @@ package wooteco.exception;
 
 public class RetrospectiveException extends RuntimeException {
 
-    private final String message;
-
     public RetrospectiveException(String message) {
-        this.message = message;
+        super(message);
     }
-
-    public String getMessage() {
-        return message;
-    }
-
 }
