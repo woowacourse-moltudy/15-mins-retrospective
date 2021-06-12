fc8fe8ba40d85a58cc09a590c9b210b37d25e68a
Daeun Lee <leede0418@likelion.org>
2021-06-12 14:09:04 +0900

    style(login): 클래스 마지막 줄 개행 삭제

--- backend/src/main/java/wooteco/exception/NotFoundException.java
+++ backend/src/main/java/wooteco/exception/NotFoundException.java
@@ -5,5 +5,4 @@ public class NotFoundException extends RetrospectiveException {
     public NotFoundException(String message) {
         super(message);
     }
-
 }
--- backend/src/main/java/wooteco/exception/member/MemberNotFoundException.java
+++ backend/src/main/java/wooteco/exception/member/MemberNotFoundException.java
@@ -7,5 +7,4 @@ public class MemberNotFoundException extends NotFoundException {
     public MemberNotFoundException() {
         super("해당 회원을 찾을 수 없습니다.");
     }
-
 }
