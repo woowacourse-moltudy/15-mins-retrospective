e2c3479b3c5bdee501fab1b2231efb589259617b
sihyung92 <fjzjqhdl@gmail.com>
2021-06-20 11:07:19 +0900

    feat(timezone-setting) : 빈 로딩 후 타임존 세팅이 KST가 아닐 경우 수정하는 로직 작성

--- backend/src/main/java/wooteco/retrospective/Application.java
+++ backend/src/main/java/wooteco/retrospective/Application.java
@@ -1,10 +1,31 @@
 package wooteco.retrospective;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
+    Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @PostConstruct
+    public void timezoneSetup() {
+        TimeZone serverDefaultTimeZone = TimeZone.getDefault();
+        TimeZone seoulTimeZone = TimeZone.getTimeZone("Asia/Seoul");
+        if (!serverDefaultTimeZone.hasSameRules(seoulTimeZone)) {
+            TimeZone.setDefault(seoulTimeZone);
+            logger.info(
+                String.format("TimeZone Setting Changed : %s -> %s",
+                              serverDefaultTimeZone.getDisplayName(),
+                              seoulTimeZone.getDisplayName()
+                )
+            );
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
