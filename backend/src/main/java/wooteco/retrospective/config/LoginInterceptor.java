eee4b18c1003a0af67270560db3f4c29573386a4
sihyung92 <fjzjqhdl@gmail.com>
2021-06-20 10:15:20 +0900

    fix(timezone) : 테스트용 임시 브랜치 생성 (타임존 확인)

--- backend/src/main/java/wooteco/retrospective/config/LoginInterceptor.java
+++ backend/src/main/java/wooteco/retrospective/config/LoginInterceptor.java
@@ -9,7 +9,11 @@ import wooteco.retrospective.utils.auth.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
+import java.util.TimeZone;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
@@ -21,7 +25,22 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-
+        System.out.println(new Date().toGMTString());
+        System.out.println(new Date().getTimezoneOffset());
+        java.util.TimeZone tz = java.util.TimeZone.getDefault();
+        System.out.println("Timezone offset from UTC reported as " +
+                (tz.getRawOffset() / 1000 / 60) + " minutes");
+        if (tz.getRawOffset() % (15 * 60 * 1000) != 0) {
+            System.out.println("Warning: not a multiple of quarter-hours");
+        }
+        System.out.println(new java.util.Date());
+        System.out.println(tz);
+        TimeZone tz2;
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss (z Z)");
+        tz2 = TimeZone.getTimeZone("Asia/Seoul");
+        df.setTimeZone(tz2);
+        System.out.format("%s%n%s%n%n", tz2.getDisplayName(), df.format(date));
         if (HttpMethod.OPTIONS.name().equals(request.getMethod())) {
             return true;
         }
