85af25fd169ae6891ff39f9b023334f1a17aba68
sihyung92 <fjzjqhdl@gmail.com>
2021-06-20 10:28:09 +0900

    fix(timezone) : 테스트용 임시 브랜치 생성 (타임존 확인)2

--- backend/src/main/java/wooteco/retrospective/config/LoginInterceptor.java
+++ backend/src/main/java/wooteco/retrospective/config/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package wooteco.retrospective.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,7 +18,8 @@ import java.util.Optional;
 import java.util.TimeZone;
 
 public class LoginInterceptor implements HandlerInterceptor {
-
+    Logger logger = LoggerFactory.getLogger(LoginInterceptor.class); 
+    
     private final JwtTokenProvider jwtTokenProvider;
 
     public LoginInterceptor(JwtTokenProvider jwtTokenProvider) {
@@ -25,22 +28,22 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println(new Date().toGMTString());
-        System.out.println(new Date().getTimezoneOffset());
+        logger.info(new Date().toGMTString());
+        logger.info(String.valueOf(new Date().getTimezoneOffset()));
         java.util.TimeZone tz = java.util.TimeZone.getDefault();
-        System.out.println("Timezone offset from UTC reported as " +
+        logger.info("Timezone offset from UTC reported as " +
                 (tz.getRawOffset() / 1000 / 60) + " minutes");
         if (tz.getRawOffset() % (15 * 60 * 1000) != 0) {
-            System.out.println("Warning: not a multiple of quarter-hours");
+            logger.info("Warning: not a multiple of quarter-hours");
         }
-        System.out.println(new java.util.Date());
-        System.out.println(tz);
+        logger.info(String.valueOf(new Date()));
+        logger.info(String.valueOf(tz));
         TimeZone tz2;
         Date date = new Date();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss (z Z)");
         tz2 = TimeZone.getTimeZone("Asia/Seoul");
         df.setTimeZone(tz2);
-        System.out.format("%s%n%s%n%n", tz2.getDisplayName(), df.format(date));
+        logger.info(String.format("%s%n%s%n%n", tz2.getDisplayName(), df.format(date)));
         if (HttpMethod.OPTIONS.name().equals(request.getMethod())) {
             return true;
         }
