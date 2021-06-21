07421157d975e8de3c4792b0ade50ed26ddc727d
Solji Choi <bssj9307@gmail.com>
2021-06-21 19:30:20 +0900

    feat(logout): 로그아웃 구현 - 로그아웃 클릭 시 토큰 삭제 + 로그인 페이지로 이동

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -10,7 +10,8 @@ class Main extends React.Component {
     this.state = {
       member: "",
       token: "",
-      times: []
+      times: [],
+      toggle: "none"
     }
   }
 
@@ -18,6 +19,23 @@ class Main extends React.Component {
     this.setState({
       token: localStorage.getItem('token')
     })
+  }
+
+  toggle = () => {
+    if (this.state.toggle === 'none') {
+      this.setState({
+        toggle: 'block'
+      })
+    } else {
+      this.setState({
+        toggle: 'none'
+      })
+    }
+  }
+
+  logout = () => {
+    localStorage.removeItem('token')
+    this.props.history.push('/login')
   }
 
   async getTimes() {
@@ -69,7 +87,8 @@ class Main extends React.Component {
             <StInfo>
               <StImg src="/logo.png"/>
               <StHead>15분 회고</StHead>
-              <StName>{this.state.member.name}</StName>
+              <StName onClick={this.toggle}>{this.state.member.name}</StName>
+              <StToggle onClick={this.logout} style={{display:this.state.toggle}}>로그아웃</StToggle>
             </StInfo>
             <StRule>
               <StRuleItem>✔ ️오후 6시, 10시 중 원하는 시간을 선택</StRuleItem>
@@ -164,6 +183,30 @@ const StName = styled.div`
   font-size: 1.2rem;
   position: absolute;
   right: 0;
+`
+
+const StToggle = styled.div`
+  font-size: 1.1rem;
+  position: absolute;
+  right: 0;
+  top: 1.5rem;
+  padding: 7px;
+
+  background: #fdfdfd;
+  border-radius: 5px;
+
+  animation: swipe ease 1s;
+
+  @keyframes swipe {
+    0% {
+      opacity: 0;
+      transform: translateY(-0.4rem);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
 `
 
 const StRule = styled.div`
